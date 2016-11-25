fb5cf91ed512a5eb57a4f459067c68efc5d2300c
Midori <bender1511@gmail.com>
2016-11-25 17:05:20 +0300

    Commented excessive logging

--- src/birch.cpp
+++ src/birch.cpp
@@ -177,11 +177,11 @@ std::vector<CF_Vector> distrKMeans(const CF_Vector &entries, size_t dim)
         for (int j = 0; j < k; ++j)
         {
             MPI_Allreduce(&localN[j], &globalN[j], 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-            LOG("Total number of points in %d cluster: %d", j + 1, globalN[j]);
+//            LOG("Total number of points in %d cluster: %d", j + 1, globalN[j]);
             MPI_Allreduce(&localSum[j][0], &globalSum[j][0], dim, MPI_DATA_T, MPI_SUM, MPI_COMM_WORLD);
             if (globalN[j] != 0)
                 centroids[j] = CF_Cluster(globalSum[j] / (data_t) globalN[j]);
-            LOG("New %d cluster: %s", j + 1, pointToString(centroids[j].X0).c_str());
+//            LOG("New %d cluster: %s", j + 1, pointToString(centroids[j].X0).c_str());
         }
         MPI_Allreduce(&localMSE, &newMSE, 1, MPI_DATA_T, MPI_SUM, MPI_COMM_WORLD);
         LOG("New MSE: %f", newMSE);
